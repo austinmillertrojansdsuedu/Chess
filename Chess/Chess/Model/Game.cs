87eaf317c3c5feb80b6f65584e4105c2d9471ac0
barkotefuye <defuye@hmc.edu>
2022-11-23 00:02:03 +0300

    Classes from UML Diagram

---
+++ Chess/Chess/Model/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Model
+{
+    
+    public class Game
+    {
+
+        public Game()
+        {
+            throw new NotImplementedException();
+        }
+        public bool makeMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isEnd()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void time()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void startGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool staleMate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool checkMate()
+        {
+            throw new NotImplementedException();
+        }
+        
+   }
+
+}
